59bc4947c4789c3db2498a9a2b48a50e9eeda565
Ko Hyunmin <junseol86@gmail.com>
2019-09-07 16:38:21 +0900

    Edit text

--- src/controllers/Order.ts
+++ src/controllers/Order.ts
@@ -11,7 +11,7 @@ class Order {
 
   setRoutes () {
     this.app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      res.send("두개 컨테이너 돌리기 한번 더 확인!" + new Date());
+      res.send("이것까지 되면 레알 감동" + new Date());
     });
   }
 }
