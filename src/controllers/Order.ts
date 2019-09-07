4d18c05189f86fed41ff92cac0a41954149f524c
Ko Hyunmin <junseol86@gmail.com>
2019-09-07 13:59:31 +0900

    Pipeline test

--- src/controllers/Order.ts
+++ src/controllers/Order.ts
@@ -8,6 +8,7 @@ class Order {
     this.setRoutes();
   }
 
+  
   setRoutes () {
     this.app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
       res.send("Hello 메시지가 바뀌었엉" + new Date());
